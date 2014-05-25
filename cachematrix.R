e116db1d14ff1e36e6606b42d7a0da23b80174fb
adeem73 <adeem.malik@gmail.com>
2014-05-25 19:59:36 +0100

    Programming Assignment 2
    Two functions are included as instructed, “makeCacheMatrix()” and
    “cacheSolve(x, …)”

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## By: Adeem Malik
+## The first function, "makeCacheMatrix()" returns a list of 4 functions, for setting and getting a matrix as well as 
+## setting and getting the inverse of the matrix. The second function, "cacheSolve(x,...)" first checks if the inverse
+## already exists in the cache, if so, it will not compute inverse again and will return the cached inverse.
 
-## Write a short comment describing this function
-
+## This function returns a list of 4 functions, for setting and getting a matrix as well as 
+## setting and getting the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        x_inv <- matrix()
+        
+        setmatrix <- function(y) {
+                x <<- y
+        }
+        
+        getmatrix <- function() x
+        
+        setinverse <- function(y) {
+                x_inv <<- solve(y)
+        }
+        
+        getinverse <- function() x_inv
+        
+        list(setmatrix = setmatrix, getmatrix = getmatrix, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## The function first checks if the inverse
+## already exists in the cache, if so, it will not compute inverse again and will return the cached inverse.
+## a null matrix will be returned if no matrix is 'set', however if a matrix is 'set' using the "setmatrix()"
+## function, running "cacheSolve()" will return inverse - inverse will be computed if it doesn't already exist
+## in cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+
+        if(!is.na(inv[1,1])) {
+                message("getting cached inverse of matrix..")
+
+                return(inv)
+        }
+        
+        matrix <- x$getmatrix()
+        
+        inv <- x$setinverse(matrix)
+        
+        inv
 }
